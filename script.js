80ce29c6d158849ae79e5f66b4e6c399a828a201
Azman <khairul.azman.rahmat@gmail.com>
2021-07-01 16:58:44 +0800

    arranged letters onto the letter board dynamically

--- script.js
+++ script.js
@@ -1,53 +1,81 @@
 class Word {
     constructor(letters) {
         this.letters = letters.toUpperCase();
+        this.uniqueChar = []; //initialise array to store all my unique characters
+        this.charIndex = {};
+        this.startWord = [];
         //this.arrIndexLetters = arrIndexLetters;        
     }
 
     getUniqueLetters() {
         const arrStrings = this.letters.split("");
-        let uniqueChar = []; //initialise array to store all my unique characters
+        //let uniqueChar = []; 
   
-        for (let i =0; i<arrStrings.length; i++) {
-          if (uniqueChar.indexOf(arrStrings[i]) == -1) {
-            uniqueChar.push(arrStrings[i]);
+        for (let i=0; i<arrStrings.length; i++) {
+          if (this.uniqueChar.indexOf(arrStrings[i]) == -1) {
+            this.uniqueChar.push(arrStrings[i]);
             }
         }
-        return uniqueChar;
-    }
 
-    getCharIndex() {
-        const uniqueChars = this.getUniqueLetters();
-        
-        let charIndex = {};
-        
-        for (let i=0; i<uniqueChars.length; i++) {
+
+        for (let i=0; i<this.uniqueChar.length; i++) {
             let indices = [];
             for (let j=0; j<this.letters.length; j++) {
-                if(this.letters[j] == uniqueChars[i]) {
+                if(this.letters[j] == this.uniqueChar[i]) {
                     indices.push(j);
                 }
             }
-            charIndex[uniqueChars[i]] = indices;
+            this.charIndex[this.uniqueChar[i]] = indices;
         }
-            
+             
         
-
-        return charIndex;
-
     }
 
     getStartWord() {
-        const uniqueChar = this.getUniqueLetters();
-        let startWord = this.letters;
+        this.startWord = this.letters;
 
-        for (let i=0; i<uniqueChar.length; i++) {
-            if (uniqueChar[i] !== " ") {
-                startWord = startWord.replaceAll(uniqueChar[i],"_");
+        for (let i=0; i<this.uniqueChar.length; i++) {
+            if (this.uniqueChar[i] !== " ") {
+                this.startWord = this.startWord.replaceAll(this.uniqueChar[i],"_");
             } 
         }
-        //startWord = startWord.split("").join(".");
-        return startWord;
+        //this.startWord = startWord.split("").join(".");
+    }
+
+    arrangeLetters() {
+        let displayObject = {row1: [], row2:[], row3:[], row4:[], row5:[]};
+        let rowNum = 1;
+        let currentRowRemainder = 13;
+        const splitWords = this.startWord.split(" ");
+
+        for (let i = 0; i < splitWords.length; i++) {
+            if (currentRowRemainder - splitWords[i].length >= 0) {
+            displayObject[`row${rowNum}`].push(splitWords[i]);
+            currentRowRemainder -= splitWords[i].length+1; //+1 to account for blank space between each word
+            } else {
+            rowNum++;
+            currentRowRemainder = 13;
+            displayObject[`row${rowNum}`].push(splitWords[i]);
+            currentRowRemainder -= splitWords[i].length+1; //+1 to account for blank space between each word
+            }
+        }
+
+        const noRows = 5;
+        //const noBlocksPerRow = 13;
+        
+        for (let i=1; i<=noRows; i++) {
+            console.log(displayObject["row"+i]);
+            let joinedWords = displayObject["row"+i].join(" ");
+            const row = document.querySelector("#row"+i); 
+            for (let j=0; j<joinedWords.length; j++) {
+                
+                if (joinedWords[j] !== " ") {                
+                row.querySelectorAll(".square")[j].style["background-color"] = "#67c50a";                           
+                }
+            }
+        }
+
+
     }
 
 
@@ -55,17 +83,35 @@ class Word {
 
 
 function makeSquares() {
-    for (let i=1; i<=135; i++) {
+    const noRows = 5;
+    const noBlocksPerRow = 13;
+    let newRow;
+    
+    for (let i=1; i<=noRows; i++) {
+        newRow = document.createElement("div");
+        newRow.setAttribute("id", ("row"+i));
+        newRow.style.display = "flex";
+        document.querySelector('#words-container').append(newRow);
+        
+        for (let j=1; j<=noBlocksPerRow; j++) {
         const square = document.createElement('div');
-        square.setAttribute("squareNo",i);
-        square.className = "square";
-        square.style.width = "28px";
-        square.style.height = "28px";    
-        document.querySelector('#words-container').append(square);
+        square.className = "square";           
+        document.querySelector("#row"+i).append(square);
+        }
     }
 }
 
 makeSquares();
+
+const originalWord = "The Phantom Of The Opera And The Giant Beaver";
+
+const wordWOF = new Word(originalWord);
+console.log(wordWOF.letters);
+wordWOF.getUniqueLetters();
+wordWOF.getStartWord();
+wordWOF.arrangeLetters();
+
+//console.log(wordWOF.startWord);
 
 // const originalWord = "Wheel of Fortune";
 // const wordWOF = new Word(originalWord);
